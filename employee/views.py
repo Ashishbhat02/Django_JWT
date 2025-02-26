9238d336a3d344d7403574404c06e4cdcc886783
Ashishbhat02 <bhatashish755@gmail.com>
2025-02-26 17:28:07 +0530

    Updated programe

--- employee/views.py
+++ employee/views.py
@@ -4,14 +4,17 @@ from rest_framework.response import Response
 from rest_framework import generics
 from .models import employee_model
 from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny
-from django.contrib.auth import authenticate
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth import authenticate 
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer , LoginSerializer , UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
 class DetailsViews(APIView):
-    def get(self, rewuest):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
         data = User.objects.all()
         details_serializer = UserSerializer(data , many = True)
         return Response(details_serializer.data)
@@ -34,14 +37,14 @@ class LoginView(generics.CreateAPIView):
         if user is not None:
             refresh = RefreshToken.for_user(user)
             user_serializer = UserSerializer(user)
-            data = User.objects.all()
-            details_serializer = UserSerializer(data , many = True)
+            # data = User.objects.all()
+            # details_serializer = UserSerializer(data , many = True)
             
             return Response({
                 'refresh' : str(refresh),
                 'access': str(refresh.access_token),
-                # 'user':user_serializer.data,
-                'user':details_serializer.data,
+                'user':user_serializer.data,
+                # 'user':details_serializer.data,
             })
         else:
             return Response({'detail': 'invalid credentials'})
